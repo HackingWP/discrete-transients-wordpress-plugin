7a2a0891302e81e44c893d18564a234738af316d
Martin Adamko <martin@attitude.sk>
2013-06-24 18:22:27 +0300

    Fix licence :)

--- include.php
+++ include.php
@@ -9,7 +9,7 @@ Author:         Martin Adamko
 Author URI:     http://www.attitude.sk
 License:        The MIT License (MIT)
 
-Copyright (c) <year> <copyright holders>
+Copyright (c) 2013 Mgr. art. Martin Adamko
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
